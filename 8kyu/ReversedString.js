ad9227abdc6322b35b4d0b3712cfabec9ece7e28
Facu <bestjayceeu@gmail.com>
2022-08-10 23:52:58 +0200

    Complete the solution so that it reverses the string passed into it

---
+++ 8kyu/ReversedString.js
@@ -0,0 +1,8 @@
+/*
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+const solution = str => str.split('').reverse().join('');
