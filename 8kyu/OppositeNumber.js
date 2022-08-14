d627252308d53b580fbc887c0ee3e14379d78eca
Facu <bestjayceeu@gmail.com>
2022-08-14 23:28:09 +0200

    given an integer or a floating-point number, find its opposite.

---
+++ 8kyu/OppositeNumber.js
@@ -0,0 +1,13 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+https://www.codewars.com/kata/56dec885c54a926dcd001095
+*/
+
+const opposite = n => n * -1;
