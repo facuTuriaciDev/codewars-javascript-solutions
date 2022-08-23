ccd41b66e00c43b0380ccb24afcb19c5df64d88e
Facu <bestjayceeu@gmail.com>
2022-08-23 23:58:35 +0200

    Remove the spaces from the string, then return the resultant string.

---
+++ 8kyu/RemoveStringSpaces.js
@@ -0,0 +1,7 @@
+/*
+Simple, remove the spaces from the string, then return the resultant string.
+
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+*/
+
+const noSpace = x => x.split(' ').join('');
