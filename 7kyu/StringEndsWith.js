d2f7ecce427aa7fb0a6023ef95ee9df444e2df3f
Facu <bestjayceeu@gmail.com>
2022-09-02 23:57:12 +0200

    Returns true if the first arg ends with the 2nd argument.

---
+++ 7kyu/StringEndsWith.js
@@ -0,0 +1,12 @@
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+
+https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+*/
+
+const solution = (str, ending) => str.substring(str.length - ending.length, str.length).includes(ending);
