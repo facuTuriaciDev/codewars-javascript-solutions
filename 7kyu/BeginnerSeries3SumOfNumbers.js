47ce7b4dd69f92c328e352dbf66ee33bdbf76a0b
Facu <bestjayceeu@gmail.com>
2022-09-03 19:14:17 +0200

    Find the sum of all the ints between and including them and return it

---
+++ 7kyu/BeginnerSeries3SumOfNumbers.js
@@ -0,0 +1,28 @@
+/*
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+https://www.codewars.com/kata/55f2b110f61eb01779000053
+*/
+
+function getSum(a, b) {
+
+  if (b < a) [a, b] = [b, a];
+
+  let res = 0;
+
+  for (var i = a; i <= b; i++) {
+    res += i;
+  }
+
+  return res;
+}
