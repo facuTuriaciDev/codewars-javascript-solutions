35328d0501128ab7915a4e5f0d70396ea6ed7148
Facu <bestjayceeu@gmail.com>
2022-09-12 13:37:20 +0200

    Returns a list of all the powers of 2 with exponent ranging from 0 to n

---
+++ 8kyu/PowersOf2.js
@@ -0,0 +1,20 @@
+/*
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+https://www.codewars.com/kata/57a083a57cb1f31db7000028
+*/
+
+function powersOfTwo(n) {
+  let res = [];
+
+  for (var i = 0; i < n + 1; i++) {
+    res.push(Math.pow(2, i));
+  }
+
+  return res;
+}
