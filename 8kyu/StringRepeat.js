b0125d6268b7371a1531d2bd5ee1e832a12ca59b
Facu <bestjayceeu@gmail.com>
2022-09-19 00:14:53 +0200

    Accepts an int and a string and repeats the strigs x times the int

---
+++ 8kyu/StringRepeat.js
@@ -0,0 +1,11 @@
+/*
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+*/
+
+const repeatStr = (n, s) => s.repeat(n);
