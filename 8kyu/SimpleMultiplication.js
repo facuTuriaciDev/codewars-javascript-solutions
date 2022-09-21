039e00db840fd9ee8a1c3c3db2b3493f3abab905
Facu <bestjayceeu@gmail.com>
2022-09-21 22:43:46 +0200

    Multiplying a number by eight if it is an even number and by nine if odd

---
+++ 8kyu/SimpleMultiplication.js
@@ -0,0 +1,8 @@
+/*
+This kata is about multiplying a given number by eight 
+if it is an even number and by nine otherwise.
+
+https://www.codewars.com/kata/583710ccaa6717322c000105
+*/
+
+const simpleMultiplication = number => number % 2 ? number * 9 : number * 8;
