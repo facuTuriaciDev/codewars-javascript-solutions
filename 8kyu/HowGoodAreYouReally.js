54aafb6452f5218a578a2ed207c16c089e0de6c4
Facu <bestjayceeu@gmail.com>
2022-09-23 23:59:25 +0200

    Return True if you're better, else False!

---
+++ 8kyu/HowGoodAreYouReally.js
@@ -0,0 +1,18 @@
+/*
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average 
+point you may add your point to the given array!
+
+https://www.codewars.com/kata/5601409514fc93442500010b
+*/
+
+function betterThanAverage(classPoints, yourPoints) {
+  return classPoints.reduce((a, b) => a + b, 0) / classPoints.length >= yourPoints ? false : true;
+}
