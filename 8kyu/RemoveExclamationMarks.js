88c796f8a577da91636a9e2b9fe3afeb4ba00187
Facu <bestjayceeu@gmail.com>
2022-09-23 23:59:50 +0200

    Removes all exclamation marks from a given string.

---
+++ 8kyu/RemoveExclamationMarks.js
@@ -0,0 +1,8 @@
+/*
+Write function RemoveExclamationMarks which removes 
+all exclamation marks from a given string.
+
+https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+*/
+
+const removeExclamationMarks = s => s.replace(/!/g, '');
