663b8d21aec45d08a3018961195d3f6098744754
Facu <bestjayceeu@gmail.com>
2022-09-24 23:28:40 +0200

    Returns an array of integers from n to 1 where n>0.

---
+++ 8kyu/ReversedSequence.js
@@ -0,0 +1,17 @@
+/*
+Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+
+https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+*/
+
+const reverseSeq = n => {
+  let arr = [];
+
+  while (n > 0) {
+    arr.push(n--);
+  }
+
+  return arr;
+};
