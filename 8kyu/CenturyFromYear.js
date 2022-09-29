aad6607d11b74b0f41a46b028dab0c067dd28796
Facu <bestjayceeu@gmail.com>
2022-09-29 23:49:57 +0200

    Given a year, return the century it is in.

---
+++ 8kyu/CenturyFromYear.js
@@ -0,0 +1,26 @@
+/*
+Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+
+https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097
+*/
+
+function century(year) {
+  let yearToCentury = Math.floor(year / 100);
+  return (year / 100) - yearToCentury === 0 ? yearToCentury : yearToCentury + 1;
+}
+
+/*
+const century = year => Math.ceil(year/100)
+
+https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
+*/
