037cffd00e5ecdfc73c2a446b366709d2ef9d94a
Facu <bestjayceeu@gmail.com>
2022-10-17 22:32:29 +0200

    Calculate if the walk the app gives you will take you exactly ten mins

---
+++ 6kyu/TakeATenMinutesWalk.js
@@ -0,0 +1,33 @@
+/*
+You live in the city of Cartesia where all roads are laid out in a perfect grid. 
+You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. 
+The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you 
+an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) 
+and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes 
+(you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). 
+It will never give you an empty array (that's not a walk, that's standing still!).
+
+https://www.codewars.com/kata/54da539698b8a2ad76000228
+*/
+
+function isValidWalk(walk) {
+  if (walk.length !== 10) {
+    return false;
+  }
+
+  let x = 0;
+  let y = 0;
+
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n': y += 1; break;
+      case 's': y -= 1; break;
+      case 'w': x -= 1; break;
+      case 'e': x += 1; break;
+    }
+  }
+
+  return y === 0 && x === 0;
+}
