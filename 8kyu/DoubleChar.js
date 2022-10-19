7c42cf4ed71831867777ae168d2a3814ba2da77d
Facu <bestjayceeu@gmail.com>
2022-10-19 23:40:50 +0200

    Return a string in which each char (case-sensitive) is repeated once.

---
+++ 8kyu/DoubleChar.js
@@ -0,0 +1,13 @@
+/*
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+  * "String"      -> "SSttrriinngg"
+  * "Hello World" -> "HHeelllloo  WWoorrlldd"
+  * "1234!_ "     -> "11223344!!__  "
+Good Luck!
+
+https://www.codewars.com/kata/56b1f01c247c01db92000076
+*/
+
+const doubleChar = str => str.split('').map(e => e.repeat(2)).join('');
