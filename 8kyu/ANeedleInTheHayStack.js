50498f101cff3b3adb6db6a2729bba2c559935b3
Facu <bestjayceeu@gmail.com>
2022-11-08 21:41:45 +0100

    Write a function findNeedle() that takes an array full of junk

---
+++ 8kyu/ANeedleInTheHayStack.js
@@ -0,0 +1,17 @@
+/*
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5"
+
+https://www.codewars.com/kata/56676e8fabd2d1ff3000000c
+*/
+
+const findNeedle = haystack => `found the needle at position ${haystack.indexOf("needle")}`;
