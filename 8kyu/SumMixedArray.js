9bbabd98a0cd17b4976ad65509271ad2372da8c3
Facu <bestjayceeu@gmail.com>
2022-11-13 23:28:39 +0100

    Return the sum of the array values as if all were numbers.

---
+++ 8kyu/SumMixedArray.js
@@ -0,0 +1,9 @@
+/*
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number.
+
+https://www.codewars.com/kata/57eaeb9578748ff92a000009
+*/
+
+const sumMix = (x) => x.reduce((a, b) => Number(a) + Number(b), 0);
